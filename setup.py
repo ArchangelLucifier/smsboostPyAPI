3b46a697231acee0b4df0ee0aa9113b58bb828f3
root <Archangel.Ariil@underground.onion>
2019-01-07 20:32:33 +0300

    release

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='smsboostPyAPI',
+    version='0.1',
+    packages=['smsboostAPI'],
+    url='https://github.com/ArchangelLucifier/smsboostPyAPI',
+    license='',
+    author='ArchangelLucifier',
+    author_email='Telegram: @ArchangelLucifier',
+    description='Python library for smsboost.ru api',
+    install_requires=[
+        "requests==2.11.1",
+    ])
